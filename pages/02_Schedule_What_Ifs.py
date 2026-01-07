5b55fd40bbf281ec17c5ba0c88b04c74d49a41ae
eaguiar34 <emilianoaguiar34@gmail.com>
2026-01-07 13:57:43 -0800

    Update 02_Schedule_What_Ifs.py

--- pages/02_Schedule_What_Ifs.py
+++ pages/02_Schedule_What_Ifs.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import fieldflow_core as core
 
-st.set_page_config(page_title="FieldFlow • Schedule What-Ifs", page_icon="🦺", layout="wide")
+st.set_page_config(page_title="Schedule What-Ifs", page_icon="📅", layout="wide")
 core.render_sidebar("Schedule What-Ifs")
-
-# Run page
 core.schedule_whatifs_page()
-
