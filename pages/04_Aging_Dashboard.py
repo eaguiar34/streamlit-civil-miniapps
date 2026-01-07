0fe3fc05f347ecf566e86c3bba2527b6cbb7cb38
eaguiar34 <emilianoaguiar34@gmail.com>
2026-01-07 13:25:26 -0800

    Update 04_Aging_Dashboard.py

--- pages/04_Aging_Dashboard.py
+++ pages/04_Aging_Dashboard.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import fieldflow_core as core
+
+st.set_page_config(page_title="FieldFlow • Aging Dashboard", page_icon="🦺", layout="wide")
+core.render_sidebar("Aging Dashboard")
+
+# Run page
+core.aging_dashboard_page()
 
