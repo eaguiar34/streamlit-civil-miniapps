4b2cad254e24d1deecdb4ea37253d27cb778ae00
eaguiar34 <emilianoaguiar34@gmail.com>
2026-01-13 21:04:42 -0800

    Update 01_Submittal_Checker.py

--- pages/01_Submittal_Checker.py
+++ pages/01_Submittal_Checker.py
@@ -2,6 +2,4 @@ import streamlit as st
 import fieldflow_core as core
 
 st.set_page_config(page_title="Submittal Checker", page_icon="📄", layout="wide")
-core.render_sidebar("Submittal Checker")
-
 core.submittal_checker_page()
