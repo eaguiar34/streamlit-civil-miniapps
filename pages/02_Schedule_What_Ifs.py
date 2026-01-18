54772daee5cfea61303d21c972bb766e18085453
eaguiar34 <emilianoaguiar34@gmail.com>
2026-01-17 21:27:33 -0800

    Update 02_Schedule_What_Ifs.py

--- pages/02_Schedule_What_Ifs.py
+++ pages/02_Schedule_What_Ifs.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import fieldflow_core as core
 
-st.set_page_config(page_title="Schedule What-Ifs", page_icon="📅", layout="wide")
-core.render_sidebar("Schedule What-Ifs")
-core.schedule_whatifs_page()
+st.set_page_config(page_title="RFI Manager", page_icon="❓", layout="wide")
+core.render_sidebar("RFI Manager")
+core.rfi_manager_page()
