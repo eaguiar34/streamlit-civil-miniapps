5f35bceb656fcdef8315dc20a624734ea54ffd65
eaguiar34 <emilianoaguiar34@gmail.com>
2026-01-17 21:29:22 -0800

    Update 06_Saved_Results.py

--- pages/06_Saved_Results.py
+++ pages/06_Saved_Results.py
@@ -2,4 +2,5 @@ import streamlit as st
 import fieldflow_core as core
 
 st.set_page_config(page_title="Saved Results", page_icon="🗂️", layout="wide")
+core.render_sidebar("Saved Results")
 core.saved_results_page()
