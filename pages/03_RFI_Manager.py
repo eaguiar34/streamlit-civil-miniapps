91ac6fc69cb5e9e0b9ce5a13340088f04326f104
eaguiar34 <emilianoaguiar34@gmail.com>
2026-01-19 10:22:07 -0800

    Update 03_RFI_Manager.py

--- pages/03_RFI_Manager.py
+++ pages/03_RFI_Manager.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import fieldflow_core as core
 
-st.set_page_config(page_title="RFI Manager", page_icon="❓", layout="wide")
+st.set_page_config(page_title="RFI Manager", page_icon="\U0001f4ec", layout="wide")
 core.render_sidebar("RFI Manager")
 core.rfi_manager_page()
